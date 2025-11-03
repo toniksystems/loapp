32c45a7416dbb5c0be81aa30e71b52294664ecdc
toniksystems <30408697+toniksystems@users.noreply.github.com>
2025-11-03 05:34:13 -0800

    Commit from Dualite Alpha:

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,9 @@ import AuditLogPage from './pages/AuditLogPage';
 
 // A wrapper for the login page to apply its specific background
 const LoginPage = () => {
-  const backgroundImageUrl = "https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://img-wrapper.vercel.app/image?url=https://s3-alpha-sig.figma.com/img/6eeb/d522/eebcb06a93a708321dc86db398604082?Expires=1762732800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=BrDbermd7yird64EneCPNXcJOceKBlh5ts-OQb0VL2EKZ7fCgGQfdu44~vQaMveq3WyAt9CzqeIP2meqpG3xo0--q2PSig8qupo8dw-gfJyxUFYHr0JXd9E1hXvDissFttlPotDe4w9ZSsHOR7SQw5cKm7I0-YnwS9G7pcnTC5C3bR5A0n0nEo1W7P0N-~lwBEeP9DCzyG21NtiQqpfEPSnGwLfJJxz1PLpRY~6Ww6DPVqaHBQBb7BGQNl5h10VzeuVMWdBKq-e9yPNClRfhv86NbEu1CSVFL~KAcEP-tF2hSwiJD0P9gZpyowG3RKbjQ~lDqo~5m4AxZPQA1PBhdQ__";
-
   return (
     <div 
-      className="min-h-screen w-full flex items-center justify-center lg:justify-end bg-cover bg-center font-ubuntu p-4 sm:p-6 lg:p-8"
-      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+      className="min-h-screen w-full flex items-center justify-center lg:justify-end bg-[url('https://img-wrapper.vercel.app/image?url=https://s3-alpha-sig.figma.com/img/6eeb/d522/eebcb06a93a708321dc86db398604082?Expires=1763337600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=hwqvYqdQpnZu5vaFJo2Sr2SZHtV9WARXbAh41t8809xP4rKSXU7kMWoIbWVsXlEnVqZl8LOs61BEDMVjNOVKyrjjpwsCo1EESvMcxAxGRa5U7Uzc8DABW-EKQIDSzryw3OMO~ihBY5kKhzvpngo~~vVBpTnJQ3KCe7HYiHGU53HFU0soYJOdLPW6QF3gGWX1rw7eOPBzY6kaXRg~9GmACtnasjzZjzgY-256ZL3j0OinAfhBQRYPJz7~l0qt5LTE~yRG3s2i444OEj0qdzxaPNCr4bRSbsZoyrGkT1OVdjx~lKxzbBHAvQ4YvWbGHHQfM6FmWBJ-SJsA65yezfSXnA__')] bg-cover bg-center bg-no-repeat font-ubuntu p-4 sm:p-6 lg:p-8"
     >
       <div className="w-full lg:w-1/2 lg:mr-20 xl:mr-32">
         <Login />
